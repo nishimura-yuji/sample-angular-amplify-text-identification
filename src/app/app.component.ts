05287777f1e4cb60c163e760d2cedfa8d603ecb4
nishimura-yuji <nishimura.yuji@classmethod.jp>
2020-01-21 01:54:58 +0900

    feat: Text identification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import Amplify from 'aws-amplify';
+import awsconfig from 'src/aws-exports';
+
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import Predictions from '@aws-amplify/predictions';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'text-from-image';
+  readText$;
+
+  onChangeInput(files: FileList) {
+    console.log(files);
+    const file = files.item(0);
+    this.identify(file);
+  }
+
+  identify(file: File) {
+    const data$ = from(
+      Predictions.identify({
+        text: { source: { file }, format: 'PLAIN' }
+      })
+    );
+
+    this.readText$ = data$.pipe(map(data => data.text.fullText));
+  }
 }
